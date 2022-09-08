8cf75dbda2cffac51cf501d648b832823c005796
deborah <omolewod@gmail.com>
2022-09-08 08:05:30 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+/**
+ *
+ *  *  main - Entry point
+ *
+ *   *  Description: prints "\"Programming is like building a multilingual puzzle"
+ *
+ *    *  Return: 0
+ *
+ *     */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
