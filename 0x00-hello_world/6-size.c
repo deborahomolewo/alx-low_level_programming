40cb33adfa76c32ceb80006bcab8da4be6ea68d0
deborah <omolewod@gmail.com>
2022-09-08 09:38:28 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - Prints sizes of characters
- * Description: using the main function
- * Return: Always 0 (success)
+ * main - print the string in the put functin
+ *
+ * Description: using the main fuction this program prints prints the size of various types on the computer it is compiled and run on
+ * Return: 0
  */
 int main(void)
 {
@@ -12,11 +14,8 @@ int main(void)
 	long long lli;
 	float f;
 
-
-	printf("Size of a char: %li byte(s)\n", sizeof(c));
-	printf("Size of a int: %li byte(s)\n", sizeof(i));
-	printf("Size of a long int: %li byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %li byte(s)\n", sizeof(lli));
-	printf("Size of a float: %li byte(s)\n", sizeof(f));
-	return (0);
-}
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n," sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n," sizeof(f));
