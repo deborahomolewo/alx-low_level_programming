6a39d889c1c7dff24f8a0ad545ae39c971af0eed
deborah <omolewod@gmail.com>
2022-09-13 04:04:18 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - this prints a-z
+ * Description: Write a function that prints the alphabet,
+ * in lowercase, followed by a new line
+ * Return: 0 (success)
+ */
+
+int print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
