aeacca771aca5b8d1a7d663de2d36be93699155e
deborah <omolewod@gmail.com>
2022-09-13 10:49:46 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	for (a = 0; a <= 9 a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
