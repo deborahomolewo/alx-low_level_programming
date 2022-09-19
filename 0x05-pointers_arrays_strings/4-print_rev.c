ca712bd7d9aa663c4d0eef4f5aebf4fb87fa72a7
deborah <omolewod@gmail.com>
2022-09-19 11:04:18 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string backwards
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	_putchar('\n');
+}
